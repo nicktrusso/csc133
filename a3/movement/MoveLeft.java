ec7fa16d1f7395591de90236a8249b0ae0cc54bd
Nick Trusso <cobbtuned@comcast.net>
2015-11-08 10:09:16 -0800

    Create MoveLeft.java

---
+++ a3/movement/MoveLeft.java
@@ -0,0 +1,28 @@
+package a3.movement;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+import a3.controller.GameWorld;
+import a3.objects.GameObjectCollection;
+import a3.objects.Net;
+
+@SuppressWarnings("serial")
+public class MoveLeft extends AbstractAction {
+	GameObjectCollection goCollection;
+	GameWorld gw;
+	
+	public MoveLeft(){
+		super("Left");
+		goCollection = GameObjectCollection.getGameObjectCollection();
+		gw = GameWorld.getGameWorld();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Net n = (Net)goCollection.getCollection().get(0);
+		System.out.println("You move the net left");
+		n.moveLeft();
+		gw.stateChange();
+	}
+}
